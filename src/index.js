f132067aad82915421366ebfd4e1bdb49f889910
Valeriya2511 <finsternis.ru@mail.ru>
2023-01-18 13:48:07 +0600

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let absol = Math.abs(n)
+    const numberLength = absol.toString().length;
+    let result = '';
+      for (let i = 0; i < numberLength; i++) {
+        let str = absol.toString();
+        result = `${str[i]}${result}`
+      }
+  return result
 }
